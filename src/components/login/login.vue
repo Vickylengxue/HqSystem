cbaa69cd8f3253289efa5375b7a8f0196511d035
Vickylengxue <vicky_lengxue@sina.com>
2017-04-25 11:12:40 +0800

    样式-login页面（差首页背景图）

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,18 +2,16 @@
 	<div class="login">
 	    <div class="login-header">
 	      <div class="title">
-	      	<p class="en-title">HQMS</p>
+	      	<p class="en-title">H<span>Q</span>MS</p>
 	      	<p class="zh-title">排队叫号管理系统</p>
 	      </div>
 	    </div>
 	    <vue-form :state="formstate" @submit.prevent="saveForm" class="saveForm">
 	        <validate  class="form-group">
-	          <span>用户名</span>
-	          <input v-model="form.userName" required name="userName" class="form-control"  :class="[fieldClassName(formstate.userName)]"/>
+	          <input v-model="form.userName" required name="userName" placeholder="用户名" class="form-control"  :class="[fieldClassName(formstate.userName)]"/>
 	        </validate>
 	        <validate  class="form-group">
-	          <span>密码</span>
-	          <input v-model="form.passwd" class="form-control" name="passwd" type="password" required :class="[fieldClassName(formstate.passwd)]" />
+	          <input v-model="form.passwd" class="form-control" name="passwd" placeholder="密码" type="password" required :class="[fieldClassName(formstate.passwd)]" />
 	        </validate>
 	        <button type="submit" class="btn btn-primary btn-block">登录</button>
 	      </vue-form>
@@ -75,37 +73,60 @@
 	}
 </script>
 
-<style scoped>
-.login-header {
-	height: 280px;
-	background: #0097FB;
-	width:100%;
-}
-.title {
-	width:500px;
-	margin:0 auto;
-	text-align: center;
-	position: relative;
-	top:50%;
-	transform: translateY(-50%);
-}
-.en-title {
-	font-family: 'Arial-Black';
-	color: #fff;
-	font-size: 52px;
-	height:73px;
-}
-.zh-title {
-	font-family: 'PingFangSC-Regular';
-	color: #fff;
-	font-size: 30px;
-	height:42px;
-	text-shadow: 0 2px 4px;
-}
-.saveForm {
-	width:400px;
-	margin:20px auto 0;
-}
+<style lang="stylus" scoped>
 
+.form-group 
+	font-size: 40px
+	margin-bottom: 1.6em
+	.form-control 
+		font-size: 28px
+		height: 80px
+
+.form-group:first-child 
+	margin-bottom: 0.6em
+
+.login-header 
+	height: 280px
+	background: #0097FB
+	width:100%
+
+.title 
+	width:500px
+	margin:0 auto
+	text-align: center
+	position: relative
+	top:50%
+	transform: translateY(-50%)
+	
+.en-title 
+	font-family: 'Arial-Black'
+	color: #fff
+	font-size: 52px
+	height:73px
+	text-shadow: 0px 2px 4px rgba(0,0,0,.1)
+
+
+.en-title span 
+	color: #47CAF9
+
+.zh-title 
+	font-family: 'PingFangSC-Regular'
+	color: #fff
+	font-size: 30px
+	height:42px
+	text-shadow: 0px 2px 4px rgba(0,0,0,.1)
+
+button
+	background-image: linear-gradient(45deg, #109EFC 0%, #00C8FF 100%)
+	box-shadow: 0 10px 20px 0 rgba(7,180,254,0.20)
+	border-radius: 5px
+	border: 0
+	font-size: 36px
+	font-weight: 200
+	height: 80px
+
+.saveForm 
+	width: 25.89%
+	margin: 1.75em auto
 
 </style>
