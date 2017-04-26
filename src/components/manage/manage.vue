46ae6cf6af9830e949ca72825fb2a2826318e3f6
Vickylengxue <vicky_lengxue@sina.com>
2017-04-26 16:24:02 +0800

    样式-manage.vue-tab切换样式+布局

--- src/components/manage/manage.vue
+++ src/components/manage/manage.vue
@@ -1,8 +1,19 @@
 <template lang="html">
 	<div class="manage">
-	    <div class="manage-header">
-            <ul class="clearfix">
-	            <li @click="goToState('stationList')" class="manage-tab">分诊台</li>
+	    <div class="manage-header container-fluid">
+            <ul class="row clearfix">
+	            <li @click="goToState('stationList')" id="fenzhentai" class="col-xs-6 col-sm-3 col-md-2 col-lg-2 manage-tab">
+		            <input id="tag1" type="radio" name="tags"/><label for="tag1"><i class="iconfont icon-fenzhentai"></i>分诊台</label>
+				</li>
+				<li @click="goToState('stationList')" id="fenzhentai" class="col-xs-6 col-sm-3 col-md-3 col-lg-3 manage-tab">
+					<input id="tag2" type="radio" name="tags"/><label for="tag2"><i class="iconfont icon-shujufenxi"></i>数据分析</label>
+				</li>
+				<li @click="goToState('stationList')" id="fenzhentai" class="col-xs-6 col-sm-3 col-md-3 col-lg-3 manage-tab">
+					<input id="tag3" type="radio" name="tags"/><label for="tag3"><i class="iconfont icon-paibanguanli"></i>排班管理</label>
+				</li>
+				<li @click="goToState('stationList')" id="fenzhentai" class="col-xs-6 col-sm-3 col-md-3 col-lg-3 manage-tab">
+					<input id="tag4" type="radio" name="tags"/><label for="tag4"><i class="iconfont icon-weixinguanli"></i>微信管理</label>
+				</li> 
             </ul>
 	    </div>
 	    <keep-alive>
@@ -22,22 +33,58 @@
 	}
 </script>
 
-<style scoped>
-.manage .manage-header {
-	height:120px;
-	background:#0097FB;
-}
-.manage .clearfix {
-	position: relative;
-	top:50%;
-	transform: translateY(-50%);
-	padding-left: 50px;
-}
-.manage-tab {
-	float: left;
-	color:#fff;
-	font-size: 36px;
-	height:50px;
+<style lang="stylus" scoped>
+
+.manage .manage-header 
+	height: 120px
+	background-image: linear-gradient(45deg, #2A82F3 0%, #00B0FF 100%)
+	display: flex
+	align-items: center
+	
+.clearfix
+	width: 1073px
+	@media screen and (max-width: 850px)
+		// width 100%
+		margin: 0 auto
+		padding: 0
+    	
+.iconfont
+	font-size: 48px
+	margin-right: 15px
+	@media (max-width: 1078px)
+		font-size: 40px
+	
+.manage-tab
+	font-family: PingFangSC-Regular
+	float: left
+	color:#fff
+	font-size: 36px
+	padding: 0
+	input
+		display: none
+	label
+		opacity: 0.5
+		display: flex
+		align-items: center
+		font-weight: normal
+		
+	label:hover
+		opacity: 1
+		font-weight: medium
+		i
+			font-weight: normal
+	@media (max-width: 1078px)
+		margin: 0
+		padding: 0
+		font-size: 20px
+		
+@media (min-width: 1078px)	
+	.manage-tab:first-child
+		margin-right: 1.2em
+	
+			
+input[type=radio]:checked + label {
+    opacity: 1
 }
 
 </style>
